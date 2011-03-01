0b83cfbde004b8d468281f2ecc3040261fe097a8
christopherross <christopherross@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-03-01 20:21:54 +0000

    git-svn-id: https://plugins.svn.wordpress.org/easy-popular-posts/trunk@353865 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- easy-popular-posts.php
+++ easy-popular-posts.php
@@ -6,7 +6,7 @@ Description: An easy to use WordPress function to add popular posts to any theme
 Author: Christopher Ross
 Tags: future, upcoming posts, upcoming post, upcoming, draft, Post, popular, preview, plugin, post, posts
 Author URI: http://thisismyurl.com
-Version: 1.7
+Version: 1.7.1
 */
 
 /*
@@ -38,49 +38,196 @@ Version: 1.7
 */
 
 
-register_activation_hook( __FILE__, 'cr_easy_popular_posts_activate' );
-add_action( 'wp_footer', 'cr_easy_popular_posts_footer_code' );
+
+
+
+// plugin definitions
+load_plugin_textdomain('epp',false, dirname(plugin_basename(__FILE__) ) . '/languages');
+load_plugin_textdomain('common',false, dirname(plugin_basename(__FILE__) ) . '/languages');
+
+
+
+
+global $cr_wplink;
+global $cr_wpname;
+$cr_wpname = "Easy Popular Posts";
+$cr_wplink = "wordpress-php-info";
+
+
+
+// add menu to WP admin
+
+function cr_epp_menu() {add_management_page('Easy Popular Posts', 'Easy Popular Posts', 10,'cr_epp.php', 'cr_epp_options');}
+add_action('admin_menu', 'cr_epp_menu');
+
+
+
+
+
+// add a comment to the footer
+
+function cr_epp_footer_code($options='') {echo "<!--  Easy Popular Posts Plugin for WordPress by Christopher Ross  - http://thisismyurl.com  -->";}
+add_action('wp_footer', 'cr_epp_footer_code');
+
+
+// add plugin functions
+
+function cr_epp_plugin_actions($links, $file){
+	static $this_plugin;
+
+	if( !$this_plugin ) $this_plugin = plugin_basename(__FILE__);
+
+	if( $file == $this_plugin ){
+		$settings_link = '<a href="tools.php?page=cr_epp.php">' . __('Settings') . '</a>';
+		$links = array_merge( array($settings_link), $links); // before other links
+	}
+	return $links;
+}
+add_filter('plugin_action_links', 'cr_epp_plugin_actions', 10, 2);
+
+
+
+
+// options page for plugin
+
+function cr_epp_options($options='') {
+	
+?>
+    
+<div class="wrap">
+
+	<a href="http://thisismyurl.com/"><div id="cross-icon" style="background: url('<?php echo WP_PLUGIN_URL .'/'.str_replace(basename( __FILE__),"",plugin_basename(__FILE__));?>/icon.png') no-repeat;" class="icon32"><br /></div></a>
+	<h2><?php echo __('Easy Popular Posts by Christopher Ross');?></h2>
+	
+
+
+	<div class="postbox-container" style="width:70%;">
+		<div class="metabox-holder">	
+		<div id="normal-sortables" class="meta-box-sortables">
+
+
+		
+			<div id="wpsettings" class="postbox">
+			<div class="handlediv" title="Click to toggle"><br /></div>
+			<h3 class="hndle"><span><?php echo __('Settings');?></span></h3>
+			<div class="inside">
+				<p><?php echo __('This plugin has no settings. To use the plugun effectively, please consult the');?> <a target='_blank' href='<?php echo WP_PLUGIN_URL .'/'.str_replace(basename( __FILE__),"",plugin_basename(__FILE__)).'readme.txt';?>'><?php echo __('readme file');?></a>.</p>
+			</div>
+			</div>
+			
+			
+			
+			
+			
+			
+			
+		</div>
+		</div>
+	</div>
+	
+
+	<?php include(WP_PLUGIN_DIR .'/'.str_replace(basename( __FILE__),"",plugin_basename(__FILE__)).'common.php');?>
+	
+	
+			
+<?php
+}
+
+
+	
+	// add scripts to footer
+	add_filter('admin_footer', 'cr_epp_add_scripts', 10, 2);
+	// add scripts to header
+	add_filter('admin_head', 'cr_epp_add_scripts_head', 10, 2);
+	
+	
+	
+	function cr_epp_add_scripts(){
+		if ($_GET['page'] == "cr_epp.php") {
+	   ?>
+	   
+	   
+	   
+	   <style>
+	   		.row {padding: 5px; font-size: .75em;}
+			.odd {background: #efefef;}
+			.even {background: #ffffff;}
+			
+			.key {width: 30%; float: left;}
+			.value {width: 70%; float: left;}
+	   </style>
+	   
+	   <script type='text/javascript' src='http://localhost:8888/wp-admin/load-scripts.php?c=1&amp;load=hoverIntent,common,jquery-color,wp-ajax-response,wp-lists,jquery-ui-core,jquery-ui-resizable,admin-comments,jquery-ui-sortable,postbox,dashboard,thickbox,plugin-install,media-upload&amp;ver=1c33e12a06a28402104d18bdc95ada53'></script>
+	
+	
+	
+	   <?php
+		}
+	}
+	
+	
+	
+	function cr_epp_add_scripts_head(){
+		if ($_GET['page'] == "cr_epp.php") {
+		?>
+		
+		
+		
+		<script type="text/javascript">
+		//<![CDATA[
+		addLoadEvent = function(func){if(typeof jQuery!="undefined")jQuery(document).ready(func);else if(typeof wpOnload!='function'){wpOnload=func;}else{var oldonload=wpOnload;wpOnload=function(){oldonload();func();}}};
+		var userSettings = {
+				'url': '/',
+				'uid': '2',
+				'time':'1296327223'
+			},
+			ajaxurl = 'http://localhost:8888/wp-admin/admin-ajax.php',
+			pagenow = 'settings_page_cr_epp',
+			typenow = '',
+			adminpage = 'settings_page_cr_epp',
+			thousandsSeparator = ',',
+			decimalPoint = '.',
+			isRtl = 0;
+		//]]>
+		</script>
+		<link rel='stylesheet' href='http://localhost:8888/wp-admin/load-styles.php?c=1&amp;dir=ltr&amp;load=dashboard,plugin-install,global,wp-admin&amp;ver=030f653716b08ff25b8bfcccabe4bdbd' type='text/css' media='all' />
+		<script type='text/javascript'>
+		/* <![CDATA[ */
+		var quicktagsL10n = {
+			quickLinks: "(Quick Links)",
+			wordLookup: "Enter a word to look up:",
+			dictionaryLookup: "Dictionary lookup",
+			lookup: "lookup",
+			closeAllOpenTags: "Close all open tags",
+			closeTags: "close tags",
+			enterURL: "Enter the URL",
+			enterImageURL: "Enter the URL of the image",
+			enterImageDescription: "Enter a description of the image"
+		};
+		try{convertEntities(quicktagsL10n);}catch(e){};
+		/* ]]> */
+		</script>
+		<script type='text/javascript' src='http://localhost:8888/wp-admin/load-scripts.php?c=1&amp;load=jquery,utils,quicktags&amp;ver=b50ff5b9792a9e89a2e131ad3119a463'></script>
+	
+	
+	
+
+<?php
+    }
+}
+
+
 
 // Shortcode
 add_shortcode( 'popularPosts', 'popularPosts' );
 
-// Plugin Admin links
-add_filter ( 'plugin_action_links', 'cr_easy_popular_posts_action' , -10, 2 );
 
 // Widget
 add_action( 'widgets_init', create_function( '', 'return register_widget( "PopularPosts" );' ) );
 
 
-function cr_easy_popular_posts_activate() {
-	if ( ( get_option( 'cr_easy_popular_posts_email' ) + ( 1300000 ) ) < date( 'U' ) ) {
-		update_option( 'cr_easy_popular_posts_email', date( 'U' ) );
-
-		$email   = get_bloginfo( 'admin_email' );
-		$name 	 = get_bloginfo( 'name' );
-		$headers = "From: $name <$email>" . "\r\n\\";
-
-		$message  = "";
-		$message .= "Thank you for installing my Easy Popular Posts plugin on ".$name."\r\n\r\n";
-		$message .= "If you enjoy this plugin, I have over 20 more on my site. They range from plugins to help you market your blog to utilities and more. Please take a few minutes to visit http://thisismyurl.com\r\n\r\n";
-		$message .= "You can support development of this plugin by making a donation via PayPal (http://thisismyurl.com/) or even better, if you enjoy it please take a few moments and vote for it at http://wordpress.org/extend/plugins/easy-popular-posts/. Thank you again for trying my plugin on your website.";
-		$message .= "\r\n\r\nChristopher Ross\r\nhttp://thisismyurl.com/";
-
-		wp_mail( $email, 'Easy Popular Posts', $message, $headers );
-	}
-}
-
-
-function cr_easy_popular_posts_footer_code( $options = '' ) {
-	echo "<!--  Easy Popular Posts by Christopher Ross - http://thisismyurl.com   -->";
-}
-
 
 function cr_easy_popular_posts_action( $links, $file ) {
-	$pluginurl = "http://thisismyurl.com/downloads/wordpress/plugins/easy-popular-posts/";
-	$this_plugin = plugin_basename ( __FILE__ );
-	if ( $file == $this_plugin ) {
-		$links[] = "<a href='" . $pluginurl . "?" . get_bloginfo('url') . "'>Manual</a>";
-	}
 	return $links;
 }
 
@@ -133,7 +280,7 @@ function popularPosts( $options = '' ) {
     }
 
 	if ( $ns_options['credit'] ) {
-		$popular .= "<li style='opacity:0.4;filter:alpha(opacity=40);'><a href='http://thisismyurl.com/downloads/wordpress/plugins/easy-popular-posts/?source=" . urlencode(get_bloginfo('url')) . "' style='opacity:0.4;filter:alpha(opacity=40);' target='_blank'>Easy Popular Posts by Christopher Ross</a></li>";
+		$popular .= "<li style='opacity:0.4;filter:alpha(opacity=40);'><a href='http://thisismyurl.com/downloads/wordpress/plugins/easy-popular-posts/?source=" . urlencode(get_bloginfo('url')) . "' style='opacity:0.4;filter:alpha(opacity=40);' target='_blank'>".__('Easy Popular Posts by Christopher Ross')."</a></li>";
 	}
 
 	if ( $ns_options['show'] ) {
@@ -182,7 +329,7 @@ class PopularPosts extends WP_Widget {
 		$credit = strip_tags( $instance['credit'] );
         ?>
 		<p>
-			<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label>
+			<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title' ); ?>:</label>
 			<input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>" />
 		</p>
 		<p>
@@ -192,6 +339,5 @@ class PopularPosts extends WP_Widget {
         <?php
 	}
 }
-
 
 ?>
